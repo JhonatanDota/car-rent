809baf434914283733419b70a23cf6cb44339790
jhonatan <j.azevedo@ctrlplay.com.br>
2023-05-07 17:26:28 -0300

    init filters

---
+++ frontend/src/components/cars/CarsFilters.tsx
@@ -0,0 +1,7 @@
+export function CarsFilters() {
+    return(
+        <div>
+            <h1>FILTER</h1>
+        </div>
+    );
+}
--- frontend/src/components/cars/CarsList.tsx
+++ frontend/src/components/cars/CarsList.tsx
@@ -13,6 +13,7 @@ import {
   CAR_TRANSMITIONS,
 } from "../../readables/carReadables";
 import CarModel from "../../models/CarModel";
+import { CarsFilters } from "./CarsFilters";
 
 export default function CarsList() {
   const [cars, setCars] = useState<CarModel[]>([]);
@@ -29,48 +30,53 @@ export default function CarsList() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-8 w-full max-w-5xl m-auto">
-      {cars.map((car) => (
-        <div className="flex flex-col gap-y-4 box-sh shadow-2xl	">
-          <img className="w-full rounded-xl" src={car.image} alt="" />
-          <div className="flex flex-col p-2 gap-1">
-            <div className="flex flex-col gap-y-2 items-center lg:items-start">
-              <h3 className="text-xl font-bold">{car.name}</h3>
+    <div className="flex flex-col md:flex-row p-8">
+      <CarsFilters/>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-5xl m-auto">
+        {cars.map((car) => (
+          <div className="flex flex-col gap-y-4 box-sh shadow-2xl	">
+            <img className="rounded-xl" src={car.image} alt="" />
+            <div className="flex flex-col p-2 gap-1">
+              <div className="flex flex-col gap-y-2 items-center lg:items-start">
+                <h3 className="text-lg font-bold">{`${BRANDS[car.brand]} - ${
+                  car.name
+                }`}</h3>
 
-              <h3 className="text-2xl font-bold">
-                {parseMonetaryValue(car.value)}{" "}
-                <span className="text-sm"> / dia</span>
-              </h3>
-            </div>
-
-            <div className="flex p-4 justify-between">
-              <div className="flex flex-col">
-                <div className="flex items-center gap-2 text-lg">
-                  <BsSpeedometer2 />
-                  <h4>{car.kilometers}</h4>
-                </div>
-
-                <div className="flex items-center gap-2 text-lg">
-                  <BsCalendar2 />
-                  <h4>{car.year}</h4>
-                </div>
+                <h3 className="text-2xl font-bold">
+                  {parseMonetaryValue(car.value)}{" "}
+                  <span className="text-sm"> / dia</span>
+                </h3>
               </div>
 
-              <div className="flex flex-col">
-                <div className="flex items-center gap-2 text-lg">
-                  <BsGear />
-                  <h4>{CAR_TRANSMITIONS[car.transmision_type]}</h4>
+              <div className="flex p-4 justify-between">
+                <div className="flex flex-col">
+                  <div className="flex items-center gap-2 text-lg">
+                    <BsSpeedometer2 />
+                    <h4>{car.kilometers}</h4>
+                  </div>
+
+                  <div className="flex items-center gap-2 text-lg">
+                    <BsCalendar2 />
+                    <h4>{car.year}</h4>
+                  </div>
                 </div>
 
-                <div className="flex items-center gap-2 text-lg">
-                  <BsFuelPump />
-                  <h4>{FUEL_TYPES[car.fuel_type]}</h4>
+                <div className="flex flex-col">
+                  <div className="flex items-center gap-2 text-lg">
+                    <BsGear />
+                    <h4>{CAR_TRANSMITIONS[car.transmision_type]}</h4>
+                  </div>
+
+                  <div className="flex items-center gap-2 text-lg">
+                    <BsFuelPump />
+                    <h4>{FUEL_TYPES[car.fuel_type]}</h4>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
